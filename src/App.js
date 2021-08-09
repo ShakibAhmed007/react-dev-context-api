0bcfdd2bf6a2be84f978f37785a874413c652032
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-09 15:17:51 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,19 +4,18 @@ import ClickCounter from './components/ClickCounter';
 import Section from './components/Section';
 import Counter from './components/Counter';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Render Props</h1>
-      <p> Similar Functional Component should be reused </p>
-
-      <Counter
-        render={(counter, incrementCount) => (
-          <ClickCounter count={counter} incrementCount={incrementCount} />
-        )}
-      />
-
-      <Section />
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Context Api</h1>
+        <Counter
+          render={(counter, incrementCount) => (
+            <ClickCounter count={counter} incrementCount={incrementCount} />
+          )}
+        />
+        <Section />
+      </div>
+    );
+  }
 }
