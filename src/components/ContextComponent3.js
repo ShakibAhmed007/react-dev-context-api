f251d0618655a80315ca5cf9343a86a6bff1876c
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-23 17:37:45 +0600

    1 file modified

--- src/components/ContextComponent3.js
+++ src/components/ContextComponent3.js
@@ -5,7 +5,7 @@ const ContextComponent3 = () => {
   const theme = useContext(ThemeContext);
   console.log('theme ---', theme);
 
-  return <div> From Root node to component 3 {theme} theme</div>;
+  return <div> From Root node to component 3 --{theme}-- theme</div>;
 };
 
 export default ContextComponent3;
